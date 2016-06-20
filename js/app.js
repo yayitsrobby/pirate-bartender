7fe749f3aa3b8efaa8f9d9e835e2d59cd6588a14
rem{e}koh <git@remekoh.me>
2016-06-20 13:44:48 -0500

    created arrays and objects

--- js/app.js
+++ js/app.js
@@ -7,34 +7,80 @@ constructors to create objects representing the bartenders questions,
 ingredients and pantry.
 */
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-  // constructor for the questions
-  var Question = function (question, category) {
-    this.question = question;
-    this.category = category;
-  };
+    // constructor for the questions
+    var Question = function(question, category) {
+        this.question = question;
+        this.category = category;
+    };
 
-  // constructor for the ingredients
-  var Ingredients = function (ingredients) {
-    this.ingredientsList = ingredients;
-  }
+    // constructor for the ingredients
+    var Ingredients = function(ingredients) {
+        this.ingredientsList = ingredients;
+    }
 
-  // constructor for the pantry
-  var Pantry = function (ingredients) {
-    this.ingredientsList = ingredients;
-  };
+    // constructor for the pantry
+    var Pantry = function(ingredients) {
+        this.ingredientsList = ingredients;
+    };
 
-  // creates objects based on constructors
-  var strong = new Question('Do ye like yer drinks strong?', 'strong');
-  var salty = new Question('Do ye like it with a salty tang?', 'salty');
-  var bitter = new Question('Are ye a lubber who likes it bitter?', 'bitter');
-  var sweet = new Question('Would ye like a bit of sweetness with yer poison?', 'sweet');
-  var fruity = new Question('Are ye one for a fruity finish?', 'fruity');
+    //creates ingredients based on constructors
+    var strongIngredient = new Ingredients(['glug of rum', 'slug of whisky', 'splash of gin']);
+    var saltyIngredient = new Ingredients(['olive on a stick', 'salt-dusted rim', 'rasher of bacon']);
+    var bitterIngredient = new Ingredients(['shake of bitters', 'splash of tonic', 'twist of lemon peel']);
+    var sweetIngredient = new Ingredients(['sugar cube', 'spoonful of honey', 'splash of cola']);
+    var fruityIngredient = new Ingredients(['slice of orange', 'dash of cassis', 'cherry on top']);
 
+    // creates objects based on constructors
+    var strongQuestion = new Question('Do ye like yer drinks strong?', strongIngredient);
+    var saltyQuestion = new Question('Do ye like it with a salty tang?', saltyIngredient );
+    var bitterQuestion = new Question('Are ye a lubber who likes it bitter?', bitterIngredient);
+    var sweetQuestion = new Question('Would ye like a bit of sweetness with yer poison?', sweetIngredient);
+    var fruityQuestion = new Question('Are ye one for a fruity finish?', fruityIngredient);
+
+    //creates Pantry objects based on constructors
+    var strongPantry = {
+      'glug of rum': 3,
+      'slug of whisky': 3,
+      'splash of gin': 3
+    }
+
+    var saltyPantry = {
+      'olive on a stick': 3,
+      'salt-dusted rim': 3,
+      'rasher of bacon': 3
+    }
+
+    var bitterPantry = {
+      'shake of bitters': 3,
+      'splash of tonic': 3,
+      'twist of lemon peel': 3
+    }
+
+    var sweetPantry = {
+      'sugar cube': 3,
+      'spoonful of honey': 3,
+      'splash of cola': 3
+    }
+
+    var fruityPantry = {
+      'slice of orange': 3,
+      'dash of cassis': 3,
+      'cherry on top': 3
+    }
+
+    var questionsList = [strongQuestion, saltyQuestion, bitterQuestion, sweetQuestion, fruityQuestion];
+
+    for (var i = 0; i < questionsList.length; i++) {
+      prompt(questionsList[i].question);
+      console.log(questionsList[i].category);
+    }
 
 
 });
+
+
 
 
 
